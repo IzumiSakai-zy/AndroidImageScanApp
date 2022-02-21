4d5aab214efc4348157f81d7f7e8a7dcbbed26b0
IzumiSakai-zy <izumisakai@aliyun.com>
2022-02-21 20:43:47 +0800

    feature:隐藏状态栏

--- app/src/main/java/com/whu/androidimagescanapp/MainActivity.kt
+++ app/src/main/java/com/whu/androidimagescanapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.whu.androidimagescanapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         setContentView(R.layout.activity_main)
     }
 }
