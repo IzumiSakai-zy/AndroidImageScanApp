1989ca39fced3dcc5371cf3eb2b273c08081db67
izumisakai-zy <izumisakai-zy@outlook.com>
2022-02-25 21:04:55 +0800

    feature:支持拍摄图片并在屏幕上显示

---
+++ app/src/main/java/com/whu/androidimagescanapp/utils/PermissionUtil.kt
@@ -0,0 +1,12 @@
+package com.whu.androidimagescanapp.utils
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+
+object PermissionUtil {
+
+    fun checkPermission(context:Context, permission:String): Boolean =
+        ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+}
