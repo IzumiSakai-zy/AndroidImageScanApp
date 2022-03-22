af80bbaae8c738ea6d802d6ea7f60fc4c8fc3985
IzumiSakai-ZY <izumisakai@aliyun.com>
2022-03-22 14:21:13 +0800

    feature(android_front_end):搜索页recycleview完成

--- android_front_end/app/src/main/java/com/whu/androidimagescanapp/ui/SearchFragment.kt
+++ android_front_end/app/src/main/java/com/whu/androidimagescanapp/ui/SearchFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.whu.androidimagescanapp.R
+import com.whu.androidimagescanapp.adapter.SearchListAdapter
 
 class SearchFragment : Fragment() {
 
@@ -15,12 +18,49 @@ class SearchFragment : Fragment() {
         fun newInstance() = SearchFragment()
     }
 
+    private var recyclerView:RecyclerView? = null
+    private lateinit var imageResourceIndexList:List<Int>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false).let {
+            recyclerView = it.findViewById(R.id.search_recycle_view)
+            it
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setResourceIndexList()
+
+        recyclerView?.apply {
+            adapter = SearchListAdapter(imageResourceIndexList)
+            layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        }
+    }
+
+    private fun setResourceIndexList() {
+        imageResourceIndexList = mutableListOf(
+            R.drawable.search_dog_item_1,
+            R.drawable.search_dog_item_2,
+            R.drawable.search_dog_item_3,
+            R.drawable.search_dog_item_4,
+            R.drawable.search_dog_item_5,
+            R.drawable.search_dog_item_6,
+            R.drawable.search_dog_item_7,
+            R.drawable.search_dog_item_8,
+            R.drawable.search_dog_item_9,
+            R.drawable.search_dog_item_10,
+            R.drawable.search_dog_item_11,
+            R.drawable.search_dog_item_12,
+            R.drawable.search_dog_item_13,
+            R.drawable.search_dog_item_14,
+            R.drawable.search_dog_item_15,
+            R.drawable.search_dog_item_5
+        )
     }
 
 }
---
+++ android_front_end/app/src/main/java/com/whu/androidimagescanapp/viewholder/SearchItemViewHolder.kt
@@ -0,0 +1,10 @@
+package com.whu.androidimagescanapp.viewholder
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.whu.androidimagescanapp.R
+
+class SearchItemViewHolder(view:View):RecyclerView.ViewHolder(view) {
+    val imageItemView = view.findViewById<ImageView>(R.id.search_recycle_item)
+}
